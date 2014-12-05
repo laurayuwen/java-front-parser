7e3df66f887c2610afceb1c2b9ad3e7cfb7fccba
Radu Mereuta <headness13@gmail.com>
2014-12-05 15:41:44 -0600

    Adding the Eclipse Spoofax project.

---
+++ editor/java/Test/TestValidator.java
@@ -0,0 +1,12 @@
+package Test;
+
+import org.strategoxt.imp.runtime.dynamicloading.Descriptor;
+import org.strategoxt.imp.runtime.services.MetaFileLanguageValidator;
+
+public class TestValidator extends MetaFileLanguageValidator 
+{ 
+  @Override public Descriptor getDescriptor()
+  { 
+    return TestParseController.getDescriptor();
+  }
+}
